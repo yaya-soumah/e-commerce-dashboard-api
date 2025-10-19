73dd12eaa7924b1bf64ad3509cbaa043007e2965
yaya-soumah <yaya-soumah@outlook.com>
2025-10-19 15:06:46 +0800

    feat(audit-logs): add model and update user model

---
+++ src/components/audit.logs/audit.logs.model.ts
@@ -0,0 +1,53 @@
+import {
+  Table,
+  Model,
+  DataType,
+  Column,
+  ForeignKey,
+  BelongsTo,
+  AllowNull,
+} from 'sequelize-typescript'
+import { Optional } from 'sequelize'
+
+import { User } from '../../models'
+
+import { AuditType, ACTION_VALUES } from './audit.logs.types'
+import type { ActionType } from './audit.logs.types'
+
+type AuditCreateType = Optional<AuditType, 'id' | 'createdAt' | 'updatedAt'>
+
+@Table({ tableName: 'ecormmerce_audit_logs', timestamps: true })
+export class AuditLogs extends Model<AuditType, AuditCreateType> {
+  @ForeignKey(() => User)
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  declare userId: number
+
+  @AllowNull(false)
+  @Column(DataType.STRING(50))
+  declare resource: string
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.ENUM(...ACTION_VALUES),
+    defaultValue: 'change',
+  })
+  declare action: ActionType
+
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  declare recordId: number
+
+  @AllowNull(false)
+  @Column(DataType.JSON)
+  declare changes: Record<string, any>
+
+  @Column(DataType.STRING)
+  declare userAgent: string
+
+  @Column(DataType.STRING)
+  declare ipAddress: string
+
+  @BelongsTo(() => User, 'user')
+  user?: User
+}
